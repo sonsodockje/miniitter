8812d74292479be47a7f6665ef2fc54e24d7c22e
강계령 <kearung.kang@gmail.com>
2024-02-19 22:29:59 +0900

    입력창 추가

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,10 +1,17 @@
-import { Flex, Box, Card } from "@radix-ui/themes";
+import { Flex, Box, Card, TextArea, Button } from "@radix-ui/themes";
 
 const List = () => {
   return (
-    <Flex gap="2" direction="column" justify="center" align="center">
-      <Card>df</Card>
-      <Card>df</Card>
+    <Flex gap="2" direction="column" justify="center" align="center" style={{ marginTop: 18 }}>
+      <Card style={{ minWidth: 320 }}>
+        <Flex direction="column" align="end">
+          <TextArea style={{ minWidth: 300 }} placeholder="내용을 작성하세요" />
+          <Button style={{ maxWidth: 70, marginTop: 4}}>업로드</Button>
+        </Flex>
+      </Card>
+      <Card style={{ minWidth: 320 }}>
+        df
+      </Card>
     </Flex>
   );
 };
